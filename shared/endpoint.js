ebaf2272a0c84049aab758640ecfb1d834107305
Frank Carver <frank@efsol.com>
2022-01-24 16:23:57 +0000

    endpoint and URL munging

--- shared/endpoint.js
+++ shared/endpoint.js
@@ -1,6 +1,79 @@
+function unknown(s) {
+    return null
+}
+
 const Endpoint = class {
-    constructor(s) {
-        let url = new URL(s)
-        console.log(`url: JSON.stringify(url)`)
+    constructor(defaults, lookup) {
+        this.defaults = defaults || {}
+        this.defaults.protocol = this.defaults.protocol || 'http'
+        this.defaults.port = this.defaults.port || 80
+        this.defaults.host = this.defaults.host || 'localhost'
+        this.lookup = lookup || unknown
+    }
+
+    applyDefaults(spec) {
+        spec.protocol = spec.protocol || this.defaults.protocol
+        spec.host = spec.host || this.defaults.host
+        spec.port = spec.port || this.defaults.port
+        return spec
+    }
+
+    parse(s) {
+        let spec = {}
+ 
+        // protocol
+        let match = s.match(/^([a-z0-9]+):\/\//)
+        if (match) {
+            console.log(`protocol match: [${match}]`)
+            spec.protocol = match[1]
+            s = s.substring(match[0].length)
+            console.log(`after protocol, s=${s}`)
+        }
+        
+        // ip address
+        match = s.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/)
+        if (match) {
+            console.log(`ip match: [${match}]`)
+            spec.host = match[0]
+            s = s.substring(match[0].length)
+            console.log(`after ip address, s=${s}`)
+        } else {
+            // domain name
+            match = s.match(/^[A-Za-z][a-z0-9]+(\.[A-Za-z0-9]+)+/)
+            if (match) {
+                console.log(`hostname match: [${match}]`)
+                spec.host = match[0]
+                s = s.substring(match[0].length)
+                console.log(`after hostmane, s=${s}`)
+            } else {
+                // role name
+                match = s.match(/^[A-Za-z][a-z0-9]+/)
+                if (match) {
+                    console.log(`roleame match: [${match}]`)
+                    let role = match[0]
+                    spec.host = this.lookup(role)
+                    s = s.substring(match[0].length)
+                    console.log(`after rolemane, s=${s}`)
+                }
+            }
+        }
+        // port
+        match = s.match(/^:([0-9]+)/)
+        if (match) {
+            console.log(`port match: [${match}]`)
+            spec.port = match[1]
+            s = s.substring(match[0].length)
+            console.log(`after port, s=${s}`)
+        }
+
+        return spec
+    }
+
+    expand(s) {
+        let spec = this.parse(s)
+        spec = this.applyDefaults(spec)
+        return `${spec.protocol}://${spec.host}:${spec.port}/`
     }
 }
+
+module.exports = Endpoint
---
+++ tests/test_endpoint.js
@@ -0,0 +1,56 @@
+const test = require('tape')
+const Endpoint = require('../shared/endpoint.js')
+
+test('empty constructor', (t) => {
+    let e = new Endpoint()
+    t.equal(e.defaults.protocol, 'http', 'default default protocol')
+    t.end()
+})
+
+test('specify default protocol', (t) => {
+    let e = new Endpoint({protocol: 'https'})
+    t.equal(e.defaults.protocol, 'https', 'specify default protocol')
+    t.end()
+})
+
+test('all defaults', (t) => {
+    let e = new Endpoint()
+    t.equal(e.expand(''), 'http://localhost:80/', 'all default defaults')
+    t.end()
+})
+
+test('ip address', (t) => {
+    let e = new Endpoint()
+    t.equal(e.expand('1.2.3.4'), 'http://1.2.3.4:80/', 'ip address as host')
+    t.end()
+})
+
+test('ip address with port', (t) => {
+    let e = new Endpoint()
+    t.equal(e.expand('1.2.3.4:995'), 'http://1.2.3.4:995/', 'ip address with port')
+    t.end()
+})
+
+test('ip address with protocol and port', (t) => {
+    let e = new Endpoint()
+    t.equal(e.expand('banana://1.2.3.4:995'), 'banana://1.2.3.4:995/', 'ip address with protocol and port')
+    t.end()
+})
+
+test('hostname', (t) => {
+    let e = new Endpoint()
+    t.equal(e.expand('uos.ac.uk'), 'http://uos.ac.uk:80/', 'domain name as host')
+    t.end()
+})
+
+test('unrecognised role', (t) => {
+    let e = new Endpoint()
+    t.equal(e.expand('logger'), 'http://localhost:80/', 'unrecognised role')
+    t.end()
+})
+
+test('recognised role', (t) => {
+    let e = new Endpoint({}, (role)=>{return '1.2.3.4'})
+    t.equal(e.expand('logger'), 'http://1.2.3.4:80/', 'recognised role')
+    t.end()
+})
