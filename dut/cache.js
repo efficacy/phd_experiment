9201e03137023b60a2c0296edc30ea0330bc66ba
Frank Carver <frank@efsol.com>
2022-04-28 14:33:25 +0100

    add cacheing to node server to see if it holds up

--- dut/cache.js
+++ dut/cache.js
@@ -1,8 +1,30 @@
+const fs = require('fs');
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('static'))
+let base = './static'
+let cache = {}
+
+app.use((req, res, next) => {
+  let url = req.originalUrl
+  if (url == '/') url = '/index.html'
+  if (url in cache) {
+    console.log(`serving ${url} from cache`)
+    res.send(cache[url])
+  } else {
+    let path = base + url
+    console.log(`reading ${path}`)
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      cache[url] = data
+      res.send(data)
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
