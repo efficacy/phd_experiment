a644c455cc6e3d29c6d1e8bdd444b6e9646e660d
Frank Carver <frank@efsol.com>
2022-05-10 18:22:32 +0100

    a better way to stop logger and registry services

--- infra/stop_reg_log.sh
+++ infra/stop_reg_log.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ps aux |grep node | awk '
-/logger\.js/ { system("kill " $2); print "killed logger " $2}
-/registry\.js/ { system("kill " $2); print "killed registry " $2}
-'
+kill `pgrep -f logger`
+echo killed logger
+kill `pgrep -f registry`
+echo killed registry
