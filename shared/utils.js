c2c35bfbe90f16e6d24d3102d5ab4ff62c19c6f4
Frank Carver <frank@efsol.com>
2022-05-12 14:54:39 +0100

    more utils fixes

--- shared/utils.js
+++ shared/utils.js
@@ -2,7 +2,7 @@ const Utils = class {
   static prefix(text, p) {
     let ret = ''
     let lines = text.toString().split('\n')
-    for (i in lines) {
+    for (let i in lines) {
       let line = lines[i].trim()
       if (line) ret += line + '\n'
     }
