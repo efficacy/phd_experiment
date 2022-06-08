c0e8567fa2a11d75589d3ee9709f401d18d08c20
Frank Carver <frank@efsol.com>
2022-06-08 16:31:40 +0100

    change shutdown command

--- registry/registry.js
+++ registry/registry.js
@@ -29,7 +29,7 @@ function getLeaseDurationInMillis(role) {
 
 function shutdown(role, address, callback) {
   if (role in ['DUT', 'LOAD', 'CTRL']) {
-    requester.ssh(address, "sudo shutdown now", (err) => {
+    requester.ssh(address, "shutdown -h now", (err) => {
       console.log(`* sent shutdown messaage to ${role}`)
     });
   } else {
