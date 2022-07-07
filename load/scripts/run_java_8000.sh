710df1b7befc2bb7cb58f619dce64aa3b3be1053
Frank Carver <frank@efsol.com>
2022-07-07 15:36:36 +0100

    java load test script

---
+++ load/scripts/run_java_8000.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# script to run the test and notify the CTRL service when complete
+# should be provided with an argument with the URL to call back
+echo $1
+siege http://192.168.0.183:8000/index.html -r 1000
+curl -s $1
