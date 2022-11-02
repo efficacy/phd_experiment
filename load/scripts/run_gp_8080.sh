4321c059cf1e0f047b2082b21099e887b405e2b0
Frank Carver <frank@efsol.com>
2022-11-02 14:55:50 +0000

    add ssl support to siege script

--- load/scripts/run_gp_8080.sh
+++ load/scripts/run_gp_8080.sh
@@ -2,5 +2,5 @@
 # script to run the test and notify the CTRL service when complete
 # should be provided with an argument with the URL to call back
 echo $1
-siege http://192.168.0.183:8080/2020/11/23/what-is-sustainability/index.html -r 1000
+siege --with-ssl=/usr/bin/openssl http://192.168.0.183:8080/2020/11/23/what-is-sustainability/index.html -r 1000
 curl -s $1
