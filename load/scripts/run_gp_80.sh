d61be2e1dea6163fb1ab82548c37c248cb4860a9
Frank Carver <frank@efsol.com>
2023-10-10 15:40:33 +0100

    try to use variable for DUT address

--- load/scripts/run_gp_80.sh
+++ load/scripts/run_gp_80.sh
@@ -2,5 +2,6 @@
 # script to run the test and notify the CTRL service when complete
 # should be provided with an argument with the URL to call back
 echo $1
-siege --with-ssl=/usr/bin/openssl http://192.168.0.183/2020/11/23/what-is-sustainability/index.html -r 1000
+target=${ ${DUT} : '192.168.0.179'}
+siege --with-ssl=/usr/bin/openssl http://${DUT}/2020/11/23/what-is-sustainability/index.html -r 1000
 curl -s $1
